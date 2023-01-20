7fa9bc4c6bf60e93c810a5866bdcfb8e9216e98f
drs317lakshami <devrajkumar317@gmail.com>
2023-01-20 22:07:11 +0530

    Added :created game board

--- src/com/bridgelabz/tictactoeprogram/TicTacToe.java
+++ src/com/bridgelabz/tictactoeprogram/TicTacToe.java
@@ -1,10 +1,18 @@
 package com.bridgelabz.tictactoeprogram;
 
 public class TicTacToe {
-    
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Tic Tac Toe Game Program in Java!");
-    
-    }
+	char gameBoard[] = null;
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Tic Tac Toe Game Program in Java!");
+		TicTacToe gameObj = new TicTacToe();
+		gameObj.initialiseGame();
+	}
+
+	void initialiseGame() {
+		gameBoard = new char[10];
+		for (int index = 0; index < 10; index++)
+			gameBoard[index] = ' ';
+	}
 
 }
